a6ca4881e557643a9ea7f67fe6d034c6916fc9a3
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-03 15:43:51 -0500

    Add fp solution to pt1

--- scala/src/main/scala/com/pomadchin/day3/Solution.scala
+++ scala/src/main/scala/com/pomadchin/day3/Solution.scala
@@ -12,7 +12,7 @@ object Solution:
 
   def readInput: Array[String] =
     Source
-      .fromFile("src/main/resources/day3/puzzle1.txt")
+      .fromFile("src/main/resources/day3/example.txt")
       .getLines
       .toArray
 
@@ -25,8 +25,6 @@ object Solution:
     // most common bit in the corresponding position
     val sb: StringBuilder = new StringBuilder()
     
-    // in the order of the least significant char
-    // start with the last char
     cfor(0)(_ < W, _ + 1) { d =>
       val counts: Array[Int] = Array.ofDim(R + 1)
 
@@ -49,8 +47,8 @@ object Solution:
   def part2(a: Array[String]): Int =
     val aux = Array.ofDim[String](a.length)
     val (o, _) = part2msd(a, aux, 0, a.length - 1, 0, _ > _)
-    val od = binToDec(a(o))
     val (c, _) = part2msd(a, aux, 0, a.length - 1, 0, _ <= _)
+    val od = binToDec(a(o))
     val oc = binToDec(a(c))
 
     od * oc
@@ -58,9 +56,8 @@ object Solution:
   // the idea to go down msd like, recursively
   // selecting always the smaller subarray 
   def part2msd(a: Array[String], aux: Array[String], lo: Int, hi: Int, d: Int, cmp: (Int, Int) => Boolean): (Int, Int) = 
-    if(lo < hi && a.length > 1) {
-      val counts = Array.ofDim[Int](R + 2); // take care of the end of string, do we need it though?
-      var (i, r) = (0, 0)
+    if(lo < hi) {
+      val counts = Array.ofDim[Int](R + 2) // take care of the end of string, do we need it though?
 
       // compute counts
       cfor(lo)(_ <= hi, _ + 1) { i => counts(charAt(a(i), d) + 2) += 1 }
@@ -86,9 +83,51 @@ object Solution:
       else part2msd(a, aux, lo + counts(1), lo + counts(2) - 1, d + 1, cmp)
     } else (lo, hi)
 
-    
+  def part2way(a: Array[String]): Int =
+    val aux = Array.ofDim[String](a.length)
+    part2msd2way(a, aux, 0, a.length - 1, 0).map(a.apply).map(binToDec).product
 
-  // I randomly decided to practice lsd sort there, since I forgot it and in Scala loops are a mess
+  // the idea to go down msd like, recursively
+  // selecting always the smaller subarray 
+  def part2msd2way(a: Array[String], aux: Array[String], lo: Int, hi: Int, d: Int): List[Int] = 
+    if(lo < hi) {
+      val counts = Array.ofDim[Int](R + 2) // take care of the end of string, do we need it though?
+
+      // compute counts
+      cfor(lo)(_ <= hi, _ + 1) { i => counts(charAt(a(i), d) + 2) += 1 }
+
+      val countsStats = counts.clone
+
+      // compute cumulatatives
+      cfor(0)(_ < R + 1, _ + 1) { r => counts(r + 1) += counts(r) }
+
+      // fill in the aux array
+      cfor(lo)(_ <= hi, _ + 1) { i =>
+        val cidx = charAt(a(i), d) + 1 // shift to the right due to +2 in counts
+        aux(counts(cidx)) = a(i)
+        counts(cidx) += 1
+      } 
+
+      // fill in the original array
+      cfor(lo)(_ <= hi, _ + 1) { i => a(i) = aux(i - lo) } 
+      
+      // "most significant" bits
+      val ms = 
+        if(countsStats(2) > countsStats(3)) part2msd2way(a, aux, lo + counts(0), lo + counts(1) - 1, d + 1)
+        else part2msd2way(a, aux, lo + counts(1), lo + counts(2) - 1, d + 1)
+
+      // "least significant" bits
+      val ls = 
+        if(countsStats(2) <= countsStats(3)) part2msd2way(a, aux, lo + counts(0), lo + counts(1) - 1, d + 1)
+        else part2msd2way(a, aux, lo + counts(1), lo + counts(2) - 1, d + 1)
+
+      // truncated ms list
+      val tms = if(ms.size > 1) ms.tail else ms
+      ls.head :: tms
+
+    } else lo :: Nil // always keep pointer to the last subarray reached
+
+  // I randomly decided to practice lsd sort there, btw, Scala loops are a mess
   def lsdsort(a: Array[String]): Array[String] =
     val W = a(0).length
     val N = a.length
@@ -139,7 +178,19 @@ object Solution:
     var res = 0;
     cfor(0)(_ < str.length, _ + 1) { i => res = res * 2 + charAt(str, i) ^ 1 }
     res
+
+  def binToDecFl(str: String): Int =
+    str.map(_ - shift).foldLeft(0) { case (a, c) => a * 2 + c }
+
+  def binToDecFlInverted(str: String): Int =
+    str.map(_ - shift).foldLeft(0) { case (a, c) => a * 2 + c ^ 1 }
+
+  def part1f(input: List[String]) = 
+    val gr = input.transpose.map { _.groupBy(identity).toList.map(_._2).maxBy(_.length).head }.mkString
+    binToDecFl(gr) * binToDecFlInverted(gr)
     
   def main(args: Array[String]): Unit =
     println(s"Q1: ${part1(readInput)}") // 2003336
     println(s"Q2: ${part2(readInput)}") // 1877139
+    println(s"Q2way: ${part2way(readInput)}") // 1877139
+    println(s"Q1*: ${part1f(readInput.toList)}") // 2003336
