3f0c819070dfdb75f91946776ba5d35b101e50b0
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-04 11:44:04 -0500

    Add day4 solution

---
+++ scala/src/main/scala/com/pomadchin/day4/Solution.scala
@@ -0,0 +1,63 @@
+package com.pomadchin.day4
+
+import scala.io.Source
+import scala.annotation.tailrec
+
+case class Input(numbers: List[Int], boards: List[Board]):
+  def part1: Int =
+    @tailrec 
+    def part10(n: List[Int], boards: List[Board]): Int =
+      val v = n.head
+      val mboards = boards.map(_.mark(v))
+      mboards.find(_.isCompleted) match
+        case Some(board) => board.score * v
+        case _ => part10(n.tail, mboards)
+
+    part10(numbers, boards)
+
+  def part2: Int = 
+    @tailrec
+    def part20(n: List[Int], boards: List[Board], acc: List[Int]): List[Int] =
+      n match
+        case Nil => acc
+        case v :: tail =>
+          val mboards = boards.map(_.mark(v))
+          mboards.find(_.isCompleted) match
+            case Some(board) => part20(tail, mboards.filterNot(_.isCompleted), board.score * v :: acc)
+            case _           => part20(tail, mboards, acc)
+
+    part20(numbers, boards, Nil).head
+
+case class Board(cells: List[List[Int]], marked: List[List[Boolean]]):
+  def mark(v: Int) = copy(marked = cells.zip(marked).map { case (row, mrow) => row.zip(mrow).map { case (c, m) => m || c == v } })
+  def score: Int = cells.zip(marked).flatMap { case (row, mrow) => row.zip(mrow).filterNot(_._2).map(_._1) }.sum
+  def isCompleted: Boolean = marked.exists(_.forall(identity)) || marked.transpose.exists(_.forall(identity))
+
+object Solution:
+  def readInput: Input =
+    val lines = 
+      Source
+        .fromFile("src/main/resources/day4/puzzle1.txt")
+        .getLines
+        .toArray
+    
+    val numbers = lines.head.split(",").map(_.toInt).toList
+
+    val boards = 
+      lines
+        .tail
+        .grouped(6)
+        .toList
+        .map(_.toList)
+        .map { list => 
+          val cells = list.map(_.split("\\W+").filter(_.nonEmpty).toList).filter(_.nonEmpty).map(_.map(_.toInt))
+          Board(cells, cells.map(_.map(_ => false)))
+        }
+
+    Input(numbers, boards)
+      
+
+  def main(args: Array[String]): Unit =
+    val input = readInput
+    println(s"Q1: ${input.part1}") // 35711
+    println(s"Q2: ${input.part2}") // 5586
