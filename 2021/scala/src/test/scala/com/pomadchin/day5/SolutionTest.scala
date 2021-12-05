b21c3f9f2f5567e6a3568f5d22bc3d143776dbae
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-05 10:49:11 -0500

    Add missing example tests

--- 2021/scala/src/test/scala/com/pomadchin/day5/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day5/SolutionTest.scala
@@ -2,7 +2,10 @@ package com.pomadchin.day5
 
 class SolutionTest extends munit.FunSuite:
   import Solution.*
-  lazy val input = readInput()
+  lazy val input        = readInput()
+  lazy val inputExample = readInput("src/main/resources/day5/example.txt")
 
+  test("Q1Example") { assertEquals(part1(inputExample), 5) }
   test("Q1") { assertEquals(part1(input), 4873) }
+  test("Q2Example") { assertEquals(part2(inputExample), 12) }
   test("Q2") { assertEquals(part2(input), 19472) }
