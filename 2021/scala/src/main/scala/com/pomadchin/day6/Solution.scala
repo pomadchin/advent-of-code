456b3863f34fe8a7120ec6e952db95e9bae57746
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-06 02:18:53 -0500

    Add day6

--- 2021/scala/src/main/scala/com/pomadchin/day6/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day6/Solution.scala
@@ -1,10 +1,59 @@
 package com.pomadchin.day6
 
 import scala.io.Source
+import scala.annotation.tailrec
 
 object Solution:
 
-  def readInput(path: String = "src/main/resources/day6/puzzle1.txt"): Iterator[String] =
+  def readInput(path: String = "src/main/resources/day6/puzzle1.txt"): List[Int] =
     Source
       .fromFile(path)
       .getLines
+      .flatMap(_.split(",").toList.map(_.toInt))
+      .toList
+
+  def part1(input: List[Int], d: Int): Int =
+    @tailrec
+    def part1rec(state: List[Int], days: Int): List[Int] =
+      days match
+        case 0 => state
+        case _ =>
+          val next = state.flatMap {
+            case 0 => 6 :: 8 :: Nil
+            case i => (i - 1) :: Nil
+          }
+          part1rec(next, days - 1)
+
+    part1rec(input, d).length
+
+  def part2(input: List[Int], d: Int): Long =
+    val initState: Map[Int, Long] =
+      input
+        .groupBy(identity)
+        .view
+        .mapValues(_.length.toLong)
+        .toMap
+
+    @tailrec
+    def part2rec(state: Map[Int, Long], days: Int): Map[Int, Long] =
+      if (days == 0) state
+      else
+        val c0 = state.get(0)
+        c0 match {
+          case Some(c0) =>
+            // if there is a zero, decrement all but zero
+            val noZerosState = (state - 0).map { (t, c) => (t - 1, c) }
+            // set add to 6 all zeros
+            // spawn extra states into the 8
+            val with68State =
+              noZerosState +
+                // there can be some 6s in the state already
+                (6 -> (noZerosState.get(6).getOrElse(0L) + c0)) +
+                (8 -> c0) // there can be no 8s, since we did the substraction before
+            part2rec(with68State, days - 1)
+          case _ =>
+            // if no zeros in the freqs state, decrement all
+            part2rec(state.map { (t, c) => (t - 1, c) }, days - 1)
+        }
+
+    part2rec(initState, d).map(_._2).sum
--- 2021/scala/src/test/scala/com/pomadchin/day6/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day6/SolutionTest.scala
@@ -5,7 +5,8 @@ class SolutionTest extends munit.FunSuite:
   lazy val input        = readInput()
   lazy val inputExample = readInput("src/main/resources/day6/example.txt")
 
-  test("Q1Example") {}
-  test("Q1") {}
-  test("Q2Example") {}
-  test("Q2") {}
+  test("Q1Example") { assertEquals(Solution.part1(inputExample, 18), 26) }
+  test("Q1") { assertEquals(Solution.part1(input, 80), 351188) }
+  test("Q2Example") { assertEquals(Solution.part2(inputExample, 18), 26L) }
+  test("Q1viaQ2") { assertEquals(Solution.part2(input, 80), 351188L) }
+  test("Q2") { assertEquals(Solution.part2(input, 256), 1595779846729L) }
