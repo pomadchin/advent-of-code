881732b305d0f800aedfdef661ec79b79e3e5a0a
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-07 14:54:43 -0500

    Add day7

--- 2021/scala/src/main/scala/com/pomadchin/day7/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day7/Solution.scala
@@ -4,7 +4,27 @@ import scala.io.Source
 
 object Solution:
 
-  def readInput(path: String = "src/main/resources/day7/puzzle1.txt"): Iterator[String] =
+  def readInput(path: String = "src/main/resources/day7/puzzle1.txt"): List[Int] =
     Source
       .fromFile(path)
       .getLines
+      .flatMap(_.split(",").map(_.toInt))
+      .toList
+      .sorted
+
+  def part1(numbers: List[Int]): Int =
+    // could be brute forced but we can compute the optimal position ahead of time - it is right in the middle
+    val mid = numbers(numbers.size / 2)
+    numbers.map { n => math.abs(n - mid) }.sum
+
+  def part2(numbers: List[Int]): Int =
+    // the min of all moves, pretty slow
+    (numbers.min to numbers.max).map { mid => // how to find it?
+      // compute moves and the sum of moves
+      numbers.map { n => math.abs(n - mid) }.map { z => z * (z + 1) / 2 }.sum
+    }.min
+
+  def part2optimized(numbers: List[Int]): Int =
+    // the target mid is the average of the sum
+    val mid = numbers.sum / numbers.length
+    numbers.map { n => math.abs(n - mid) }.map { z => z * (z + 1) / 2 }.sum
--- 2021/scala/src/test/scala/com/pomadchin/day7/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day7/SolutionTest.scala
@@ -5,7 +5,8 @@ class SolutionTest extends munit.FunSuite:
   lazy val input        = readInput()
   lazy val inputExample = readInput("src/main/resources/day7/example.txt")
 
-  test("Q1Example") {}
-  test("Q1") {}
-  test("Q2Example") {}
-  test("Q2") {}
+  test("Q1Example") { assertEquals(part1(inputExample), 37) }
+  test("Q1") { assertEquals(part1(input), 336120) }
+  test("Q2Example") { assertEquals(part2(inputExample), 168) }
+  test("Q2") { assertEquals(part2(input), 96864235) }
+  test("Q2optimized") { assertEquals(part2optimized(input), 96864235) }
