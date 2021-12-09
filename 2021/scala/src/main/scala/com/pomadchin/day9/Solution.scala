29c1f6b7924d7376b05e76f96d16d67c1bc7f555
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-09 01:30:22 -0500

    Add a mutable day9 solution

--- 2021/scala/src/main/scala/com/pomadchin/day9/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day9/Solution.scala
@@ -1,10 +1,138 @@
 package com.pomadchin.day9
 
+import com.pomadchin.util.CforMacros.cfor
+
 import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
 
 object Solution:
 
-  def readInput(path: String = "src/main/resources/day9/puzzle1.txt"): Iterator[String] =
+  opaque type Table = Array[Array[Int]]
+  object Table:
+    def apply(arr: Array[Array[Int]]): Table = arr
+
+  extension (t: Table)
+    def toArray: Array[Array[Int]]            = t
+    def get(row: Int, col: Int): Int          = toArray(row)(col)
+    def cols: Int                             = toArray(0).length
+    def rows: Int                             = toArray.length
+    def index(row: Int, col: Int): Int        = row * cols + col
+    def col(index: Int): Int                  = index % cols
+    def row(index: Int): Int                  = index / cols
+    def adj(r: Int, c: Int): List[(Int, Int)] = Solution.adj(r, c, t)
+
+  def readInput(path: String = "src/main/resources/day9/puzzle1.txt"): Table =
     Source
       .fromFile(path)
       .getLines
+      .map(_.toCharArray.map(_.asDigit))
+      .toArray
+
+  import scala.collection.mutable
+  def part1(table: Table): Int =
+    val result = mutable.ListBuffer[Int]()
+
+    cfor(0)(_ < table.rows, _ + 1) { r =>
+      cfor(0)(_ < table.cols, _ + 1) { c =>
+        val v = table.get(r, c)
+        if (table.adj(r, c).forall((r, c) => v < table.get(r, c)))
+          result += (v + 1)
+      }
+    }
+
+    result.sum
+
+  def part2(table: Table): Int =
+    val result = mutable.ListBuffer[Int]()
+    val marked = Array.ofDim[Boolean](table.rows, table.cols)
+
+    cfor(0)(_ < table.rows, _ + 1) { r =>
+      cfor(0)(_ < table.cols, _ + 1) { c =>
+        val v = table.get(r, c)
+        if (table.adj(r, c).forall((r, c) => v < table.get(r, c))) {
+          val basin = mutable.ListBuffer[Int]()
+          // add low point
+          basin += v
+          def dfs(row: Int, col: Int): Unit = {
+            // visit the current vertex
+            marked(row)(col) = true
+            table.adj(row, col).foreach { (ra, ca) =>
+              if (!marked(ra)(ca)) {
+                val va = table.get(ra, ca)
+                if (va < 9) {
+                  basin += va
+                  dfs(ra, ca)
+                }
+              }
+            }
+            // marked(row)(col) = false
+          }
+
+          dfs(r, c)
+          result += basin.toList.length
+        }
+      }
+    }
+
+    result.sortBy(e => -e).take(3).product
+
+  // unfortunately we don't need diagonals ):
+  def adj(r: Int, c: Int, table: Table, diagonal: Boolean = false): List[(Int, Int)] =
+    val result = mutable.ListBuffer[(Int, Int)]()
+    // there is a top row present
+    if (r > 0) {
+      val rt = r - 1
+      // add edge between the current and top mid
+      result += (rt -> c);
+      // top left
+      if (diagonal) {
+        if (c > 0) {
+          val cl = c - 1
+          // add edge between the current and top left
+          result += (rt -> cl)
+        }
+        // top right
+        if (c < table.cols - 1) {
+          val cr = c + 1
+          // add edge between the current and top right
+          result += (rt -> cr)
+        }
+      }
+    }
+    // current row
+    if (c > 0) {
+      val cl = c - 1
+      // add edge between the current and current left
+      result += (r -> cl)
+    }
+    // add edge between the current and top mid
+    // skip reflective vertices
+    // result += (r -> c)
+    // top right
+    if (c < table.cols - 1) {
+      val cr = c + 1
+      // add edge between the current and current right
+      result += (r -> cr)
+    }
+    // there is a bottom row present
+    if (r < table.rows - 1) {
+      val rb = r + 1
+      // add edge between the current and bottom mid
+      result += (rb -> c)
+      // bottom left
+      if (diagonal) {
+        if (c > 0) {
+          val cl = c - 1;
+          // add edge between the current and top left
+          result += (rb -> cl)
+        }
+        // top right
+        if (c < table.cols - 1) {
+          val cr = c + 1;
+          // add edge between the current and top right
+          result += (rb -> cr)
+        }
+      }
+    }
+
+    result.toList
--- 2021/scala/src/test/scala/com/pomadchin/day9/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day9/SolutionTest.scala
@@ -2,10 +2,10 @@ package com.pomadchin.day9
 
 class SolutionTest extends munit.FunSuite:
   import Solution.*
-  def input        = readInput()
-  def inputExample = readInput("src/main/resources/day9/example.txt")
+  val input        = readInput()
+  val inputExample = readInput("src/main/resources/day9/example.txt")
 
-  test("Q1Example") {}
-  test("Q1") {}
-  test("Q2Example") {}
-  test("Q2") {}
+  test("Q1Example") { assertEquals(part1(inputExample), 15) }
+  test("Q1") { assertEquals(part1(input), 502) }
+  test("Q2Example") { assertEquals(part2(inputExample), 1134) }
+  test("Q2") { assertEquals(part2(input), 1330560) }
