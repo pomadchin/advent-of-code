bad90f32179334f492ca2534bf4bd907a5436a76
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-13 09:33:15 -0500

    Add day13 solution

--- 2021/scala/src/main/scala/com/pomadchin/day13/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day13/Solution.scala
@@ -4,5 +4,55 @@ import scala.io.Source
 
 object Solution:
 
-  def readInput(path: String = "src/main/resources/day13/puzzle1.txt"): Iterator[String] =
-    Source.fromFile(path).getLines
+  case class Input(coords: Set[(Int, Int)], instructions: List[(String, Int)])
+
+  def readInput(path: String = "src/main/resources/day13/puzzle1.txt"): Input =
+    val source = Source.fromFile(path).getLines.toList
+    val coords: Set[(Int, Int)] =
+      source
+        .filter(_.contains(","))
+        .map(_.split(",").toList.map(_.toInt))
+        .flatMap {
+          case List(f, s) => Some(f -> s)
+          case _          => None
+        }
+        .toSet
+
+    val instructions: List[(String, Int)] =
+      source
+        .filter(_.startsWith("fold along "))
+        .map(_.split("fold along ").last.split("=").toList)
+        .flatMap {
+          case List(f, s) => Some(f -> s.toInt)
+          case _          => None
+        }
+
+    Input(coords, instructions)
+
+  def part1(input: Input): Int =
+    // in the first task we take only the first instruction
+    input.instructions
+      .take(1)
+      .foldLeft(input.coords) { case (acc, i) =>
+        i match
+          case ("x", v) => acc.map((x, y) => (v - math.abs(v - x), y))
+          case ("y", v) => acc.map((x, y) => (x, v - math.abs(v - y)))
+          case _        => acc
+      }
+      .size
+
+  def part2(input: Input) =
+    val result = input.instructions
+      .foldLeft(input.coords) { case (acc, i) =>
+        i match
+          case ("x", v) => acc.map((x, y) => (v - math.abs(v - x), y))
+          case ("y", v) => acc.map((x, y) => (x, v - math.abs(v - y)))
+          case _        => acc
+      }
+      .toList
+      .sorted
+
+    val (minx, miny) = result.head
+    val (maxx, maxy) = result.last
+
+    (miny to maxy).map { y => (minx to maxx).map { x => if result.contains(x, y) then "##" else "  " }.mkString }.mkString("\n")
--- 2021/scala/src/test/scala/com/pomadchin/day13/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day13/SolutionTest.scala
@@ -5,7 +5,7 @@ class SolutionTest extends munit.FunSuite:
   def input        = readInput()
   def inputExample = readInput("src/main/resources/day13/example.txt")
 
-  test("Q1Example") {}
-  test("Q1") {}
-  test("Q2Example") {}
-  test("Q2") {}
+  test("Q1Example") { assertEquals(part1(inputExample), 17) }
+  test("Q1") { assertEquals(part1(input), 751) }
+  test("Q2Example") { println(part2(inputExample)) } // prints 0
+  test("Q2") { println(part2(input)) }               // prints PGHRKLKL
