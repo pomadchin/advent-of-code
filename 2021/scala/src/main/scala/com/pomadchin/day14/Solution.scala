d32f33379ac1183f4505affa6ae4bbebc4b1fc27
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-14 11:44:39 -0500

    Add day14

--- 2021/scala/src/main/scala/com/pomadchin/day14/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day14/Solution.scala
@@ -3,6 +3,92 @@ package com.pomadchin.day14
 import scala.io.Source
 
 object Solution:
+  def readInput(path: String = "src/main/resources/day14/puzzle1.txt"): (Map[Char, Long], Map[String, Long], List[(String, Char)]) =
+    val iter     = Source.fromFile(path).getLines
+    val template = iter.next
 
-  def readInput(path: String = "src/main/resources/day14/puzzle1.txt"): Iterator[String] =
-    Source.fromFile(path).getLines
+    // preserve original counts
+    val counts =
+      template
+        .groupBy(identity)
+        .toMap
+        .view
+        .mapValues(_.length.toLong)
+        .toMap
+
+    // suffixes counts
+    val suffixes = template.sliding(2).toList.map { (_, 1) }.groupBy(_._1).view.mapValues(_.map(_._2).sum.toLong).toMap
+
+    // instructions
+    val instructions =
+      iter
+        .filter(_.contains("->"))
+        .map(_.split(" -> ").toList)
+        .flatMap {
+          case List(f, s) => Some(f -> s.head)
+          case _          => None
+        }
+        .toList
+
+    (counts, suffixes, instructions)
+
+  import scala.collection.mutable
+
+  def part1(input: (Map[Char, Long], Map[String, Long], List[(String, Char)]), steps: Int): Long =
+    val (counts, suffixes, instructions)       = input
+    val countsMutable: mutable.Map[Char, Long] = counts.to(mutable.Map)
+    var suffixesMutable                        = suffixes
+
+    // the idea on everystep to build the new suffixes array
+    // every suffix may be there more than once
+    // the number of replacements accumulate
+    (0 until steps).map { s =>
+      val suffixesNew = mutable.Map[String, Long]()
+      // for each value find a matching rule
+      suffixesMutable.foreach { (k, v) =>
+        instructions.filter(_._1 == k).foreach { (from, to) =>
+          // increment counts in the counts map
+          countsMutable.put(to, countsMutable.get(to).getOrElse(0L) + v)
+          // fill in the new suffixes array
+          val ft = s"${from.head}$to"
+          val tf = s"$to${from.last}"
+          // carefull with it
+          suffixesNew.put(ft, suffixesNew.get(ft).getOrElse(0L) + v)
+          suffixesNew.put(tf, suffixesNew.get(tf).getOrElse(0L) + v)
+        }
+      }
+
+      // use the new suffixes array
+      suffixesMutable = suffixesNew.toMap
+    }
+
+    val srt = countsMutable.toList.map(_._2)
+    srt.max - srt.min
+
+  def part1f(input: (Map[Char, Long], Map[String, Long], List[(String, Char)]), steps: Int): Long =
+    val (counts, suffixes, instructions) = input
+    // the idea on every step to build the new suffixes array
+    // every suffix may be there more than once
+    // the number of replacements accumulate (and match the amount of suffixes)
+    // folding all steps
+    val (countsNew, _) = (0 until steps).foldLeft((counts, suffixes)) { case ((counts, suffixes), _) =>
+      suffixes.foldLeft((counts, Map[String, Long]())) { case ((counts, suffixesNew), (k, v)) =>
+        instructions.filter(_._1 == k).foldLeft((counts, suffixesNew)) { case ((counts, suffixesNew), (from, to)) =>
+          // add more counts of the new target
+          val countsNew = counts + (to -> (counts.getOrElse(to, 0L) + v))
+
+          // compute new suffixes
+          val ft = s"${from.head}$to"
+          val tf = s"$to${from.last}"
+          // build new suffixes with cumulatative counts
+          (countsNew,
+           suffixesNew +
+             (ft -> (suffixesNew.getOrElse(ft, 0L) + v)) +
+             (tf -> (suffixesNew.getOrElse(tf, 0L) + v))
+          )
+        }
+      }
+    }
+
+    val srt = countsNew.toList.map(_._2)
+    srt.max - srt.min
--- 2021/scala/src/test/scala/com/pomadchin/day14/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day14/SolutionTest.scala
@@ -5,7 +5,9 @@ class SolutionTest extends munit.FunSuite:
   def input        = readInput()
   def inputExample = readInput("src/main/resources/day14/example.txt")
 
-  test("Q1Example") {}
-  test("Q1") {}
-  test("Q2Example") {}
-  test("Q2") {}
+  test("Q1Example") { assertEquals(part1(inputExample, 10), 1588L) }
+  test("Q1") { assertEquals(part1(input, 10), 2584L) }
+  test("Q1*") { assertEquals(part1f(input, 10), 2584L) }
+  test("Q2Example") { assertEquals(part1(inputExample, 40), 2188189693529L) }
+  test("Q2") { assertEquals(part1(input, 40), 3816397135460L) }
+  test("Q2*") { assertEquals(part1f(input, 40), 3816397135460L) }
