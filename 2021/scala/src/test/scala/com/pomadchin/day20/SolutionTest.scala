8ce3275ee72dfc883d26ac4b2a85c78e6aec6343
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-20 12:28:38 -0500

    Add day20 stub

---
+++ 2021/scala/src/test/scala/com/pomadchin/day20/SolutionTest.scala
@@ -0,0 +1,11 @@
+package com.pomadchin.day20
+
+class SolutionTest extends munit.FunSuite:
+  import Solution.*
+  val input        = readInput()
+  val inputExample = readInput("src/main/resources/day20/example.txt")
+
+  test("Q1Example") {}
+  test("Q1") {}
+  test("Q2Example") {}
+  test("Q2") {}
