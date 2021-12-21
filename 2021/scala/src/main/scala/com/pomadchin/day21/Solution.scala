79027515ff62038f60851b1ce4a86fed235edc37
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-21 14:16:30 -0500

    Add Day21 part 2

--- 2021/scala/src/main/scala/com/pomadchin/day21/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day21/Solution.scala
@@ -2,6 +2,7 @@ package com.pomadchin.day21
 
 import scala.io.Source
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 case class State[S, A](run: S => (A, S)):
   def map[B](f: A => B): State[S, B] = State(s =>
@@ -23,7 +24,7 @@ object Solution:
   def score(i: Int): Int = { val v = i % 10; if (v == 0) 10 else v }
 
   // [1; 100]
-  def dice: State[Int, Int] = State { i =>
+  val dice: State[Int, Int] = State { i =>
     val v = math.max(1, i % 101); (v, v + 1)
   }
 
@@ -35,7 +36,7 @@ object Solution:
     yield (d1 + d2 + d3, d3)
 
   @tailrec
-  def compute(p1: Int, p2: Int, p1score: Int, p2score: Int, step: Int, state: Int): Int =
+  def computeDeterministic(p1: Int, p2: Int, p1score: Int, p2score: Int, step: Int, state: Int): Int =
     if p1score >= 1000 then p2score * (step * 6 - 3)
     else if p2score >= 1000 then p1score * step * 6
     else
@@ -53,7 +54,43 @@ object Solution:
         val p2n                      = score(p2points + p2)
         val p2scoren                 = p2score + p2n
 
-        compute(p1n, p2n, p1scoren, p2scoren, step + 1, p2state)
+        computeDeterministic(p1n, p2n, p1scoren, p2scoren, step + 1, p2state)
+
+  // compute all possible rolls of 3
+  val allrolls =
+    for
+      i <- 1 to 3
+      j <- 1 to 3
+      k <- 1 to 3
+    yield i + j + k
+
+  // cache func calls
+  val memoizeDirac = mutable.Map.empty[(Int, Int, Int, Int), (Long, Long)]
+  def computeDirac(p1: Int, p2: Int, p1score: Int, p2score: Int): (Long, Long) =
+    memoizeDirac.getOrElseUpdate(
+      (p1, p2, p1score, p2score), {
+        if p1score >= 21 then (1, 0)
+        else if p2score >= 21 then (0, 1)
+        else
+          // everytime throw all dice
+          allrolls.foldLeft(0L -> 0L) { case ((p1winsa, p2winsa), r) =>
+            // compute p1 score first
+            var p1n = score(p1 + r)
+            // compute the summ score
+            var p1scoren = p1score + p1n
+
+            // now do the same but for the p2
+            // swap func arguments
+            val (p2wins, p1wins) = computeDirac(p2, p1n, p2score, p1scoren)
+
+            (p1winsa + p1wins, p2winsa + p2wins)
+          }
+      }
+    )
 
   def part1(p1: Int, p2: Int): Int =
-    compute(p1, p2, 0, 0, 1, 1)
+    computeDeterministic(p1, p2, 0, 0, 1, 1)
+
+  def part2(p1: Int, p2: Int): Long =
+    val (l, r) = computeDirac(p1, p2, 0, 0)
+    math.max(l, r)
--- 2021/scala/src/test/scala/com/pomadchin/day21/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day21/SolutionTest.scala
@@ -7,5 +7,5 @@ class SolutionTest extends munit.FunSuite:
 
   test("Q1Example") { assertEquals(part1.tupled(inputExample), 739785) }
   test("Q1") { assertEquals(part1.tupled(input), 906093) }
-  test("Q2Example") {}
-  test("Q2") {}
+  test("Q2Example") { assertEquals(part2.tupled(inputExample), 444356092776315L) }
+  test("Q2") { assertEquals(part2.tupled(input), 274291038026362L) }
