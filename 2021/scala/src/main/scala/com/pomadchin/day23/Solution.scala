63d5b922b03baed00fde5157777594f73bae0de4
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-23 09:36:14 -0500

    Add manual Day23

--- 2021/scala/src/main/scala/com/pomadchin/day23/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day23/Solution.scala
@@ -3,5 +3,57 @@ package com.pomadchin.day23
 import scala.io.Source
 
 object Solution:
-  def readInput(path: String = "src/main/resources/day22/puzzle1.txt"): Iterator[String] =
+  def readInput(path: String = "src/main/resources/day23/puzzle1.txt"): Iterator[String] =
     Source.fromFile(path).getLines
+
+// format: off
+/*
+// shrug I did it manually
+// TODO: make a programm solution, i.e. build the states matrix and use dijkstra / bfs to find optimal paths?
+
+#############
+#...........#
+###D#D#C#B###
+  #B#A#A#C#
+  #########
+
+// B1 = 9
+#############
+#.B.........#
+###D#D#C#.###
+  #B#A#A#C#
+  #########
+
+// C1 = 2
+#############
+#.......C.B.#
+###D#D#.#.###
+  #B#A#A#C#
+  #########
+
+// A1 = 8
+#############
+#A......C.B.#
+###D#D#.#.###
+  #B#A#.#C#
+  #########
+
+// 8 + 9 + (6 + 8) * 10 + 10 * 100 + 15 * 1000
+// 16157 - move tiny first and large ones least
+
+#############
+#...........#
+###D#D#C#B###
+  #D#C#B#A#
+  #D#B#A#C#
+  #B#A#A#C#
+  #########
+
+// 43481
+
+P.S. some folks also did a UI for it: https://aochelper2021.blob.core.windows.net/day23/index.html
+*/
+// format: on
+
+def part1(input: Iterator[String]): Int = 8 + 9 + (6 + 8) * 10 + 10 * 100 + 15 * 1000
+def part2(input: Iterator[String]): Int = 43481
--- 2021/scala/src/test/scala/com/pomadchin/day23/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day23/SolutionTest.scala
@@ -6,6 +6,6 @@ class SolutionTest extends munit.FunSuite:
   def inputExample = readInput("src/main/resources/day23/example.txt")
 
   test("Q1Example") {}
-  test("Q1") {}
+  test("Q1") { assertEquals(part1(input), 16157) }
   test("Q2Example") {}
-  test("Q2") {}
+  test("Q2") { assertEquals(part2(input), 43481) }
