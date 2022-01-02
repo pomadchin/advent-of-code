e82c01d255abf4be2d4e9d3038e092254e2ff103
Grigory Pomadchin <gr.pomadchin@gmail.com>
2022-01-02 00:18:53 -0500

    Add Day2 pt2 rec schemes solution

--- 2021/scala/src/main/scala/com/pomadchin/day2/Solution.scala
+++ 2021/scala/src/main/scala/com/pomadchin/day2/Solution.scala
@@ -6,8 +6,8 @@ object Solution:
   // rec scehemes solution
   import cats.{Functor, Monoid}
   import cats.syntax.monoid.*
-  import higherkindness.droste.{scheme, Algebra, Coalgebra}
-  import higherkindness.droste.data.Fix
+  import higherkindness.droste.{scheme, Algebra, CVAlgebra, Coalgebra}
+  import higherkindness.droste.data.*
   import higherkindness.droste.data.list.*
 
   // a higher kinded list
@@ -32,14 +32,23 @@ object Solution:
     case Nil          => StopF()
   }
 
-  case class CoordsState(x: Int, y: Int):
+  opaque type CoordsState = (Int, Int, Int)
+  object CoordsState:
+    def apply(t: (Int, Int, Int)): CoordsState     = t
+    def apply(x: Int, y: Int, a: Int): CoordsState = apply((x, y, a))
+    def apply(x: Int, y: Int): CoordsState         = apply(x, y, 0)
+
+  extension (s: CoordsState)
+    def x: Int   = s._1
+    def y: Int   = s._2
+    def a: Int   = s._3
     def res: Int = x * y
 
   given Monoid[CoordsState] = new Monoid[CoordsState]:
     def empty                                                = CoordsState(0, 0)
-    def combine(x: CoordsState, y: CoordsState): CoordsState = CoordsState(x.x + y.x, x.y + y.y)
+    def combine(x: CoordsState, y: CoordsState): CoordsState = CoordsState(x.x + y.x, x.y + y.y, x.a + y.a)
 
-  def accumulateAlgebraFunction = Algebra[DirectionF[(Position, Int), *], CoordsState => CoordsState] {
+  val accumulateAlgebraFunction = Algebra[DirectionF[(Position, Int), *], CoordsState => CoordsState] {
     // t is the state func in this case
     case DirectionConsF((pos, v), t) =>
       state =>
@@ -57,7 +66,7 @@ object Solution:
     case StopF() => identity
   }
 
-  def accumulateAlgebra = Algebra[DirectionF[(Position, Int), *], CoordsState] {
+  val accumulateAlgebra = Algebra[DirectionF[(Position, Int), *], CoordsState] {
     case DirectionConsF((pos, v), t) =>
       val x = pos match
         case FORWARD => v
@@ -68,16 +77,14 @@ object Solution:
         case DOWN => v
         case _    => 0
 
-      val state = CoordsState(x, y)
-
-      state |+| t
+      CoordsState(x, y) |+| t
 
     case StopF() => summon[Monoid[CoordsState]].empty
   }
 
   // cata recurisvely goes recursively applies algebra to every next element
   // the result is the folded (left) accumulator
-  def accumulateListFAlgebra = Algebra[ListF[(Position, Int), *], CoordsState] {
+  val accumulateListFAlgebra = Algebra[ListF[(Position, Int), *], CoordsState] {
     case ConsF((pos, v), t) =>
       val x = pos match
         case FORWARD => v
@@ -88,9 +95,41 @@ object Solution:
         case DOWN => v
         case _    => 0
 
-      val state = CoordsState(x, y)
+      CoordsState(x, y) |+| t
 
-      state |+| t
+    case NilF => summon[Monoid[CoordsState]].empty
+  }
+
+  val accumulateListFCVAlgebra = CVAlgebra[ListF[(Position, Int), *], CoordsState] {
+    // access to the previous computation step here via Attr[F[_], A]
+    // case ConsF((pos, v), prev :< ConsF(_, prev2 :< _)) =>
+    case ConsF((pos, v), prev :< _) =>
+      val a = pos match
+        case UP   => v
+        case DOWN => -v
+        case _    => 0
+
+      val (x, y) = pos match
+        case FORWARD => (v, prev.a * -v)
+        case _       => (0, 0)
+
+      CoordsState(x, y, a) |+| prev
+
+    case NilF => summon[Monoid[CoordsState]].empty
+  }
+
+  val accumulateListFAlgebra2 = Algebra[ListF[(Position, Int), *], CoordsState] {
+    case ConsF((pos, v), prev) =>
+      val a = pos match
+        case UP   => v
+        case DOWN => -v
+        case _    => 0
+
+      val (x, y) = pos match
+        case FORWARD => (v, prev.a * -v)
+        case _       => (0, 0)
+
+      CoordsState(x, y, a) |+| prev
 
     case NilF => summon[Monoid[CoordsState]].empty
   }
@@ -100,6 +139,15 @@ object Solution:
 
   def part1rec2(input: List[(Position, Int)]): Int =
     scheme.hylo(accumulateListFAlgebra, ListF.fromScalaListCoalgebra).apply(input).res
+
+  def part2rec(input: List[(Position, Int)]): Int =
+    // dyna = histo + ana
+    // WARN: reverse, we traverse through the tree the other direction
+    // we don't really need it here, since the ListF structure already keeps the tail in its structure
+    scheme.zoo.dyna(accumulateListFCVAlgebra, ListF.fromScalaListCoalgebra).apply(input.reverse).res
+
+  def part2rec2(input: List[(Position, Int)]): Int =
+    scheme.hylo(accumulateListFAlgebra2, ListF.fromScalaListCoalgebra).apply(input.reverse).res
 
   type Position = String
   val FORWARD = "forward"
--- 2021/scala/src/test/scala/com/pomadchin/day2/SolutionTest.scala
+++ 2021/scala/src/test/scala/com/pomadchin/day2/SolutionTest.scala
@@ -8,3 +8,5 @@ class SolutionTest extends munit.FunSuite:
   test("Q1*") { assertEquals(part1rec(input), 1636725) }
   test("Q1**") { assertEquals(part1rec2(input), 1636725) }
   test("Q2") { assertEquals(part2(input.iterator), 1872757425) }
+  test("Q2*") { assertEquals(part2rec(input), 1872757425) }
+  test("Q2**") { assertEquals(part2rec2(input), 1872757425) }
