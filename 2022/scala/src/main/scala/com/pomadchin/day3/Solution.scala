64af7d78114a64dc66da1e02f6fdf5302c4d8d69
Grigory Pomadchin <grigory.pomadchin@disneystreaming.com>
2022-12-03 09:59:10 -0500

    Day 3

--- 2022/scala/src/main/scala/com/pomadchin/day3/Solution.scala
+++ 2022/scala/src/main/scala/com/pomadchin/day3/Solution.scala
@@ -4,11 +4,35 @@ import scala.io.Source
 
 object Solution:
 
-  def readInput(path: String = "src/main/resources/day3/puzzle1.txt"): Iterator[String] =
+  val index = ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  opaque type Letter = Char
+  object Letter:
+    def apply(c: Char): Letter = c
+
+  extension (l: Letter)
+    def c: Char       = l
+    def priority: Int = index.indexOf(c)
+
+  opaque type Compartments = (Set[Letter], Set[Letter])
+  object Compartments:
+    def apply(t: (Set[Letter], Set[Letter])): Compartments  = t
+    def apply(f: Set[Letter], s: Set[Letter]): Compartments = f -> s
+
+  extension (t: Compartments)
+    def tupled: (Set[Letter], Set[Letter]) = t
+    def set: Set[Letter]                   = t._1 ++ t._2
+
+  def readInput(path: String = "src/main/resources/day3/puzzle1.txt"): List[Compartments] =
     Source
       .fromFile(path)
       .getLines
+      .map(line => line.splitAt(line.length() / 2))
+      .map((fst, snd) => Compartments(fst.toSet.map(Letter(_)), snd.toSet.map(Letter(_))))
+      .toList
 
-  def part1(input: Iterator[String]): Unit = ()
+  def part1(input: List[Compartments]): Int =
+    input.map((fst, snd) => fst.intersect(snd).map(_.priority).sum).sum
 
-  def part2(input: Iterator[String]): Unit = ()
+  def part2(input: List[Compartments]): Int =
+    input.grouped(3).map(_.toSet.map(_.set).reduce(_.intersect(_)).map(_.priority).sum).sum
--- 2022/scala/src/test/scala/com/pomadchin/day3/SolutionTest.scala
+++ 2022/scala/src/test/scala/com/pomadchin/day3/SolutionTest.scala
@@ -2,7 +2,10 @@ package com.pomadchin.day3
 
 class SolutionTest extends munit.FunSuite:
   import Solution.*
-  lazy val input = readInput()
+  lazy val input        = readInput()
+  lazy val inputExample = readInput("src/main/resources/day3/example.txt")
 
-// test("Q1") { }
-// test("Q2") { }
+  test("Q1Example") { assertEquals(part1(inputExample), 157) }
+  test("Q1") { assertEquals(part1(input), 8109) }
+  test("Q2Example") { assertEquals(part2(inputExample), 70) }
+  test("Q2") { assertEquals(part2(input), 2738) }
