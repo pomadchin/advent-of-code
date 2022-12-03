9e2aab0f6f6f88b8e1adc632606209251fc22175
Grigory Pomadchin <grigory.pomadchin@disneystreaming.com>
2022-12-03 18:36:44 -0500

    Code style / cleanup

--- 2022/rust/src/day03/mod.rs
+++ 2022/rust/src/day03/mod.rs
@@ -7,15 +7,23 @@ fn char_to_priority(c: char) -> i32 {
     INDEX.chars().position(|i| i == c).unwrap() as i32
 }
 
+fn input() -> Vec<String> {
+    utils::read_file_in_cwd("src/day03/puzzle1.txt")
+        .split('\n')
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect()
+}
+
 fn part1(rucksacks: &Vec<String>) -> i32 {
     rucksacks
         .into_iter()
         .map(|rucksack| {
             let (compartment_1, compartment_2) = rucksack.split_at(rucksack.len() / 2);
-            let set1 = compartment_1.chars().collect::<HashSet<_>>();
-            let set2 = compartment_2.chars().collect::<HashSet<_>>();
+            let set1: HashSet<char> = compartment_1.chars().collect();
+            let set2: HashSet<char> = compartment_2.chars().collect();
 
-            set1.intersection(&set2).map(|c| char_to_priority(*c)).sum::<i32>()
+            set1.intersection(&set2).cloned().map(char_to_priority).sum::<i32>()
         })
         .sum()
 }
@@ -24,27 +32,21 @@ fn part2(rucksacks: &Vec<String>) -> i32 {
     (0..rucksacks.len())
         .step_by(3)
         .map(|group_index| {
-            let set1 = rucksacks[group_index].chars().collect::<HashSet<_>>();
-            let set2 = rucksacks[group_index + 1].chars().collect::<HashSet<_>>();
-            let set3 = rucksacks[group_index + 2].chars().collect::<HashSet<_>>();
+            let set1: HashSet<char> = rucksacks[group_index].chars().collect();
+            let set2: HashSet<char> = rucksacks[group_index + 1].chars().collect();
+            let set3: HashSet<char> = rucksacks[group_index + 2].chars().collect();
 
             set1.intersection(&set2)
-                .map(|c| *c)
+                .cloned()
                 .collect::<HashSet<_>>()
                 .intersection(&set3)
-                .map(|c| *c)
+                .cloned()
                 .collect::<HashSet<_>>()
                 .into_iter()
-                .map(|c| char_to_priority(c))
+                .map(char_to_priority)
                 .sum::<i32>()
         })
         .sum()
-}
-
-fn input() -> Vec<String> {
-    let input = utils::read_file_in_cwd("src/day03/puzzle1.txt");
-
-    input.split('\n').into_iter().map(|s| s.to_string()).collect::<Vec<_>>()
 }
 
 #[cfg(test)]
