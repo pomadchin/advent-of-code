e4edd14d92a6e8fce36f9c8d1135335fd462c323
Grigory Pomadchin <gr.pomadchin@gmail.com>
2023-12-18 12:17:48 -0500

    Add Day 18

--- 2023/zig/src/day18/main.zig
+++ 2023/zig/src/day18/main.zig
@@ -1,40 +1,158 @@
 const std = @import("std");
 const util = @import("util");
 
-pub fn solve(input: []const u8) ![3]u32 {
-    var lines = std.mem.split(u8, input, "\n");
-    _ = lines;
+const Str = util.Str;
+const Allocator = std.mem.Allocator;
 
-    var max: [3]u32 = .{ 0, 0, 0 };
+const Point = struct {
+    y: i64,
+    x: i64,
 
-    return max;
+    pub fn add(self: *Point, other: Point) Point {
+        return Point{ .y = self.y + other.y, .x = self.x + other.x };
+    }
+
+    pub fn mul(self: *Point, d: i64) Point {
+        return Point{ .y = self.y * d, .x = self.x * d };
+    }
+};
+const Instruction = struct {
+    direction: Point,
+    dist: i64,
+    color: Str,
+
+    pub fn distc(self: *Instruction) !i64 {
+        var slice = self.color[1..6];
+        return try util.parseInt(i64, slice, 16);
+    }
+
+    pub fn directionc(self: *Instruction) Point {
+        return switch (self.color[6]) {
+            '3' => Point{ .y = -1, .x = 0 },
+            '1' => Point{ .y = 1, .x = 0 },
+            '0' => Point{ .y = 0, .x = 1 },
+            '2' => Point{ .y = 0, .x = -1 },
+            else => Point{ .y = -99, .x = -99 },
+        };
+    }
+};
+
+fn direction(c: u8) Point {
+    return switch (c) {
+        'U' => Point{ .y = -1, .x = 0 },
+        'D' => Point{ .y = 1, .x = 0 },
+        'R' => Point{ .y = 0, .x = 1 },
+        'L' => Point{ .y = 0, .x = -1 },
+        else => Point{ .y = -99, .x = -99 },
+    };
 }
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn shoelace(points: []Point, allocator: Allocator) !u64 {
+    var res: i64 = 0;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var y = try allocator.alloc(i64, points.len + 1);
+    var x = try allocator.alloc(i64, points.len + 1);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    for (points, 0..) |p, i| {
+        y[i] = p.y;
+        x[i] = p.x;
+    }
 
-    try bw.flush(); // don't forget to flush!
+    y[points.len] = points[0].y;
+    x[points.len] = points[0].x;
 
-    // const max = try solve(@embedFile("input.txt"));
-    // const total = @reduce(.Add, @as(@Vector(3, u32), max));
-    // std.debug.print("Part 1: {d}\n", .{max[0]});
-    // std.debug.print("Part 2: {any} = {d}\n", .{ max, total });
+    for (0..points.len) |i| res += x[i] * y[i + 1] - y[i] * x[i + 1];
+
+    return util.abs(res) / 2;
 }
 
-test "test-input" {
-    // const max = try solve(@embedFile("test.txt"));
-    const max = try solve(&[_]u8{ 'a', 'b', 'c' });
-    const total = @reduce(.Add, @as(@Vector(3, u32), max));
-    try std.testing.expectEqual(max[0], 0);
-    try std.testing.expectEqual(total, 0);
+fn parse(line: Str) !Instruction {
+    var split = util.splitStr(line, " ");
+    var d = direction((split.next().?)[0]);
+    var distStr = split.next().?;
+    var dist = try util.parseInt(i64, distStr, 10);
+    var colorBuf = split.next().?;
+    var color = colorBuf[1 .. colorBuf.len - 1];
+
+    return Instruction{ .direction = d, .dist = dist, .color = color };
+}
+
+pub fn part1(input: Str) !u64 {
+    var arena = util.arena_gpa;
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    var current = Point{ .y = 0, .x = 0 };
+
+    var points = std.ArrayList(Point).init(allocator);
+    defer points.deinit();
+
+    var p: u64 = 0; // border / perimiter
+
+    var lines = util.splitStr(input, "\n");
+    while (lines.next()) |line| {
+        var inst = try parse(line);
+        var dist = inst.dist;
+        var dir = inst.direction;
+        p += @as(u64, @intCast(dist));
+        current = current.add(dir.mul(dist));
+        try points.append(current);
+    }
+
+    return try shoelace(points.items, allocator) + (p / 2) + 1;
+}
+
+pub fn part2(input: Str) !u64 {
+    var arena = util.arena_gpa;
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    var current = Point{ .y = 0, .x = 0 };
+
+    var points = std.ArrayList(Point).init(allocator);
+    defer points.deinit();
+
+    var p: u64 = 0; // border / perimiter
+
+    var lines = util.splitStr(input, "\n");
+    while (lines.next()) |line| {
+        var inst = try parse(line);
+        var dist = try inst.distc();
+        var dir = inst.directionc();
+        p += @as(u64, @intCast(dist));
+        current = current.add(dir.mul(dist));
+        try points.append(current);
+    }
+
+    return try shoelace(points.items, allocator) + (p / 2) + 1;
+}
+
+pub fn main() !void {}
+
+test "example-part1" {
+    const actual = try part1(@embedFile("example1.txt"));
+    const expected = @as(u64, 62);
+
+    try util.expectEqual(expected, actual);
+}
+
+test "input-part1" {
+    const actual = try part1(@embedFile("input1.txt"));
+    const expected = @as(u64, 39194);
+
+    try util.expectEqual(expected, actual);
+}
+
+test "example-part2" {
+    const actual = try part2(@embedFile("example1.txt"));
+    const expected = @as(u64, 952408144115);
+
+    try util.expectEqual(expected, actual);
+}
+
+test "input-part2" {
+    const actual = try part2(@embedFile("input1.txt"));
+    const expected = @as(u64, 78242031808225);
+
+    try util.expectEqual(expected, actual);
 }
