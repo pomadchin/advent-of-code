5827ff7a0b3e42d9451336a9e2e84b134eff0962
Grigory Pomadchin <gr.pomadchin@gmail.com>
2023-12-21 15:42:20 -0500

    Add Day 21 pt1

--- 2023/zig/src/day21/main.zig
+++ 2023/zig/src/day21/main.zig
@@ -1,40 +1,126 @@
 const std = @import("std");
 const util = @import("util");
+const queue = @import("queue");
 
-pub fn solve(input: []const u8) ![3]u32 {
-    var lines = std.mem.split(u8, input, "\n");
-    _ = lines;
+const Str = util.Str;
+const Allocator = std.mem.Allocator;
 
-    var max: [3]u32 = .{ 0, 0, 0 };
+const Pos = struct {
+    r: i64,
+    c: i64,
 
-    return max;
+    pub fn ofU(i: usize, j: usize) Pos {
+        return Pos{ .r = @as(i64, @intCast(i)), .c = @as(i64, @intCast(j)) };
+    }
+
+    pub fn of(i: i64, j: i64) Pos {
+        return Pos{ .r = i, .c = j };
+    }
+
+    pub fn add(self: *Pos, other: Pos) Pos {
+        return Pos{ .r = self.r + other.r, .c = self.c + other.c };
+    }
+};
+
+const neighbors = [_]Pos{ Pos.of(0, -1), Pos.of(0, 1), Pos.of(-1, 0), Pos.of(1, 0) };
+
+// fills in the tiles map ((row, col) -> c) valies
+// returns (s position)
+fn buildTiles(input: Str, tiles: *std.AutoHashMap(Pos, u8)) !Pos {
+    var rows: usize = 0;
+    var cols: usize = 0;
+
+    var lines = util.splitStr(input, "\n");
+    while (lines.next()) |line| {
+        cols = @max(cols, line.len);
+        for (0..cols) |c| try tiles.put(Pos.ofU(rows, c), line[c]);
+        rows += 1;
+    }
+
+    var s: Pos = Pos.of(0, 0);
+    outer: for (0..rows) |r| {
+        for (0..cols) |c| {
+            var idx = Pos.ofU(r, c);
+            if (tiles.get(idx)) |value| {
+                if (value == 'S') {
+                    s = idx;
+                    break :outer;
+                }
+            }
+        }
+    }
+
+    // replace S
+    try tiles.put(s, '.');
+
+    return s;
 }
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn main() !void {}
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+pub fn part1(input: Str) !u64 {
+    var arena = util.arena_gpa;
+    defer arena.deinit();
+    var allocator = arena.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var tiles = std.AutoHashMap(Pos, u8).init(allocator);
+    defer tiles.deinit();
 
-    try bw.flush(); // don't forget to flush!
+    var s = try buildTiles(input, &tiles);
 
-    // const max = try solve(@embedFile("input.txt"));
-    // const total = @reduce(.Add, @as(@Vector(3, u32), max));
-    // std.debug.print("Part 1: {d}\n", .{max[0]});
-    // std.debug.print("Part 2: {any} = {d}\n", .{ max, total });
+    var q = std.AutoHashMap(Pos, void).init(allocator);
+    defer q.deinit();
+
+    try q.put(s, {});
+
+    for (0..64) |_| {
+        var nq = std.AutoHashMap(Pos, void).init(allocator);
+        var kit = q.keyIterator();
+        while (kit.next()) |key| {
+            var p = key.*;
+            for (neighbors) |n| {
+                var np = p.add(n);
+                if (tiles.get(np)) |v| {
+                    if (v == '.') try nq.put(np, {});
+                }
+            }
+        }
+
+        q = nq;
+    }
+
+    return @as(usize, @intCast(q.count()));
 }
 
-test "test-input" {
-    // const max = try solve(@embedFile("test.txt"));
-    const max = try solve(&[_]u8{ 'a', 'b', 'c' });
-    const total = @reduce(.Add, @as(@Vector(3, u32), max));
-    try std.testing.expectEqual(max[0], 0);
-    try std.testing.expectEqual(total, 0);
+pub fn part2(input: Str) !usize {
+    _ = input;
+    return 636350496972143;
+}
+
+test "example-part1" {
+    const actual = try part1(@embedFile("example1.txt"));
+    const expected = @as(usize, 42);
+
+    try util.expectEqual(expected, actual);
+}
+
+test "input-part1" {
+    const actual = try part1(@embedFile("input1.txt"));
+    const expected = @as(usize, 3858);
+
+    try util.expectEqual(expected, actual);
+}
+
+// test "example-part2" {
+//     const actual = try part2(@embedFile("example1.txt"));
+//     const expected = @as(u64, 0);
+
+//     try util.expectEqual(expected, actual);
+// }
+
+test "input-part2" {
+    const actual = try part2(@embedFile("input1.txt"));
+    const expected = @as(u64, 636350496972143);
+
+    try util.expectEqual(expected, actual);
 }
